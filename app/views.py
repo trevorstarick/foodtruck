bdee2819171d535a8c0f8a06a911b6e0fc5375b3
Trevor Starick <trevor.starick@gmail.com>
2013-05-27 12:01:24 -0400

    Initial Commit

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template("index.html",
+		title = 'Home')
+
+@app.errorhandler(404)
+def fourohfour(e):
+	return render_template("404.html",
+		title = 'Error: 404',)
+@app.errorhandler(500)
+def fivehundred(e):
+	return render_template("500.html",
+		title = 'Error: 500')
